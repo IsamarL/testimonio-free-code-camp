5f9ae60369e65ce7fd2dd5cb1601a51b86e0fb0d
IsamarL <dl335358@gmail.com>
2025-04-03 17:15:48 -0600

    Subiendo proyecto completo

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import Testimonio from './Componente/Testimonio';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.App}>
+      <StatusBar barStyle="ligth-content" backgroundColor="#3080a1" />
+        <View style={styles.contenedorPrincipal}>
+          <View style={styles.bannerCont}>
+            <Image source={require('/imagenes/imagen1.png')} style={styles.banner} />
+          </View>
+         <Text style={styles.titulo} >Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp</Text>
+          <ScrollView>
+         <Testimonio 
+            imagen={require('./imagenes/imagen2.png')}
+            nombre='Shawn Wang'
+            pais='Singapur'
+            cargo='Ingeniero de software'
+            empresa='Amazon'
+            testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere dolorem accusantium veniam perspiciatis autem harum quam est fugit molestias?'
+        />
+
+        <Testimonio
+            imagen={require('./imagenes/imagen 3.png')}
+            nombre='Emma Bostian'
+            pais='Suecia'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere dolorem accusantium veniam perspiciatis autem harum quam est fugit molestias?'
+        />
+
+        <Testimonio
+            imagen={require('./imagenes/imagen 4.png')}  
+            nombre='Sarah Chima'
+            pais='Nigger'
+            cargo='Ingeniera de software'
+            empresa='ChatDesk'
+            testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere dolorem accusantium veniam perspiciatis autem harum quam est fugit molestias?'
+        /> 
+        </ScrollView>
+        </View>
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+App:{
+  flex:1,
+  backgroundColor:'#f1f1f1'
+},
+bannerCont:{
+  height: 100,
+  width: '100%',
+},
+banner:{
+  height: 80, 
+  width: '100%',
+  resizeMode:'contain',
+  backgroundColor:'#1b1b32'
+},
+contenedorPrincipal:{
+  alignItems:'center',
+  width:'100%',
+},
+titulo:{
+  textAlign:'center',
+  fontSize:20,
+  fontWeight:'bold'
+},
+
 });
---
+++ Componente/Testimonio.js
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+
+export default function Testimonio(props) {
+  return (
+    <View style={styles.contenedorTestimonio}>
+      <View style={styles.ImgContainer}>
+        <Image source={props.imagen} style={styles.imagenTestimonio} />
+      </View>
+      <View style={styles.contenedorTextoTestimonio}>
+        <Text style={styles.nombreTestimonio}>
+          <Text style={styles.negritas}>{props.nombre}</Text>
+          <Text> en </Text>
+          <Text style={styles.pais}>{props.pais}</Text>
+        </Text>
+
+        <Text style={styles.nombreTestimonio}>
+          <Text >{props.cargo}</Text>
+          <Text> en </Text>
+          <Text style={styles.negritas}>{props.empresa}</Text>
+        </Text>
+
+        <Text style={styles.textoTestimonio}>{props.testimonio}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contenedorTestimonio: {
+    flex: 1,
+    flexDirection: "row",
+    minHeight: 250,
+    backgroundColor: "#fff",
+    marginVertical: "10px",
+    marginHorizontal: "50px",
+  },
+  ImgContainer: {
+    flex: 1,
+  },
+  imagenTestimonio: {
+    alignSelf: "center",
+    height: 250,
+    width: "100%",
+    margin: 2,
+  },
+  contenedorTextoTestimonio: {
+    flex: 2,
+    alignSelf: "center",
+    margin:4,
+  },
+  textoTestimonio: {
+    textAlign: "justify",
+    fontSize: 14,
+  },
+  negritas: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
